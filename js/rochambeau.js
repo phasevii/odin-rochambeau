de824206a852bad54f75947dace343f6cb578131
phasevii <reidulicny@gmail.com>
2023-01-30 20:58:44 -0500

    Add RNG and conditionals to getComputerChoice

--- js/rochambeau.js
+++ js/rochambeau.js
@@ -1,3 +1,13 @@
+//Chooses an outcome based on a randomly chosen number between 1 and 2
 function getComputerChoice() {
-    
+    let choice = Math.floor(Math.random() * 2) + 1;
+    if(choice = 0) {
+        return "rock";
+    }
+    else if(choice = 1) {
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
 }
