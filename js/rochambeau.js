35b53927fab4bb66ade20e0e6d7b681144e6901d
phasevii <reidulicny@gmail.com>
2023-02-01 18:49:24 -0500

    Removed call to Math.floor in getComputerChoice

--- js/rochambeau.js
+++ js/rochambeau.js
@@ -1,13 +1,15 @@
 //Chooses an outcome based on a randomly chosen number between 1 and 2
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 2) + 1;
-    if(choice = 0) {
+    let choice = Math.random();
+    if(choice <= .33) {
         return "rock";
     }
-    else if(choice = 1) {
+    else if(choice > .33 && choice <= .66) {
         return "paper";
     }
     else {
         return "scissors"
     }
 }
+
+console.log(getComputerChoice());
