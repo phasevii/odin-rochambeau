86bbe971ae21ce6ceea5a808450a727ec0e9858a
phasevii <reidulicny@gmail.com>
2023-02-01 20:28:55 -0500

    Add conditionals for handling results to playRound

--- js/rochambeau.js
+++ js/rochambeau.js
@@ -13,5 +13,31 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    if(playerSelection === "rock" && computerSelection === "paper") {
+        alert("You lose! Paper beats Rock!");
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors") {
+        alert("You win! Rock beats Scissors!");
+    }
+    else if(playerSelection === "rock" && computerSelection === "rock") {
+        alert("Tie!");
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors") {
+        alert("You lose! Scissors beat Paper!");
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock") {
+        alert("You win! Paper beats Rock!");
+    }
+    else if(playerSelection === "paper" && computerSelection === "paper") {
+        alert("Tie!")
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock") {
+        alert("You lose! Rock beats Scissors!");
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper") {
+        alert("You win! Scissors beat Paper!")
+    }
+    else if(playerSelection === "scissors" && computerSelection === "scissors") {
+        alert("Tie!")
+    }
 }
